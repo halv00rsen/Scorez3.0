c3246430974f4394e7eb82e9345d963a8a588276
halv00rsen <jorgen-halvorsen@hotmail.com>
2015-12-05 15:16:38 +0100

    Must now retype password

--- init_database.py
+++ init_database.py
@@ -1,21 +1,23 @@
 #! python3
 from server import init_db, hash_password, connect_db
+from getpass import getpass
 
 if __name__ == "__main__":
 	print("Will create a new database")
 	print("You will need an admin account.")
 	username = input("Username: ")
-	password = input("Password: ")
-	if len(username) == 0 or len(password) == 0:
+	password = getpass("Password: ")
+	retype_pass = getpass("Retype password: ")
+
+	if len(username) == 0 or len(password) == 0 or password != retype_pass:
 		print("Error! Username or password is empty.")
 	else:
 		with open("database.db", "w") as f:
 			pass
 		init_db()
-		password = hash_password(password)
 		db = connect_db()
 		try:
-			r = db.execute("insert into User values (?,?,?)", [username, password, True])
+			r = db.execute("insert into User values (?,?,?)", [username, hash_password(password), True])
 			db.execute("insert into Beer_type values (?)", ["Ingen"])
 			db.commit()
 			print("Database was successfully created.")
